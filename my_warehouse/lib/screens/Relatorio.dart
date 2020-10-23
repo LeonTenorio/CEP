e283b94f796f167b2099150bf2233af948196dcb
Malcoln Soares <malcolnsoares5000@hotmail.com>
2020-10-22 22:06:42 -0300

    Esse é o final
    This is the End

--- my_warehouse/lib/screens/Relatorio.dart
+++ my_warehouse/lib/screens/Relatorio.dart
@@ -75,19 +75,19 @@ class _RelatorioState extends State<Relatorio> {
                     Text("Item: "+ nome, style: TextStyle(fontSize: 18.0),),
                   ],
                 ),
-                Row(
+                /*Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     Text("Ganho/Custo: " + infos[3].toStringAsFixed(2).replaceAll(".", ",") + "%", style: TextStyle(fontSize: 18.0),),
                   ],
-                ),
+                ),*/
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     Text("Vendas: " + infos[0].toString(), style: TextStyle(fontSize: 18.0),),
-                    Text("BRL/unid: " + infos[4].toStringAsFixed(2).replaceAll(".", ","), style: TextStyle(fontSize: 18.0),),
+                    //Text("BRL/unid: " + infos[4].toStringAsFixed(2).replaceAll(".", ","), style: TextStyle(fontSize: 18.0),),
                   ],
                 ),
                 Row(
@@ -112,11 +112,105 @@ class _RelatorioState extends State<Relatorio> {
         ),
       ),
       onTap: (){
+        showDialog(context: context, builder: (BuildContext context){
+          return buildPopUpRelatorio(receitasVendidas, nome, infos);
+        });
         //NAO SEI COMO FICOU, MAS ACHO QUE AQUI SERIA A PAGINA DO ALE
         //Navigator.push(context, MaterialPageRoute(builder: (context) => VisualizarReceitasVendidas(nome: nome, receitasVendias: receitasVendias)));
       },
     );
   }
+
+
+  Widget buildPopUpRelatorio(List<Receita> receitas, String nome,List<dynamic> infos){
+    return AlertDialog(
+      title: Text("INFORMAÇÕES DA VENDA: "+nome, style: TextStyle(fontSize: 18.0),),
+      content: Container(
+        width: MediaQuery.of(context).size.width*0.7,
+        height: MediaQuery.of(context).size.height*0.7,
+        child: ListView(
+          shrinkWrap: true,
+          children: [
+            Card(
+              color: Colors.deepOrangeAccent,
+              child: Padding(
+                padding: EdgeInsets.all(5.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Receita: "+receitas[0].nome+ "\n", style: TextStyle(fontSize: 16.0),),
+                    Text("Ganho/Custo:\n " + infos[3].toStringAsFixed(2).replaceAll(".", ",") + "%"+"\n", style: TextStyle(fontSize: 18.0),),
+                    Text("Vendas: " + infos[0].toString()+ "\n", style: TextStyle(fontSize: 18.0),),
+                    Text("Receita total: " + infos[1].toStringAsFixed(2).replaceAll(".", ",")+ "\n", style: TextStyle(fontSize: 18.0),),
+                    Text("Lucro/unid: " + infos[5].toStringAsFixed(2).replaceAll(".", ",")+ "\n", style: TextStyle(fontSize: 18.0),),
+                    Text("Preço/unid vendida:\n " + infos[4].toStringAsFixed(2).replaceAll(".", ",")+ "\n", style: TextStyle(fontSize: 18.0),),
+                  ],
+                ),
+              ),
+            ),
+            ListView.builder(
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: receitas.length,
+              itemBuilder: (BuildContext context, int index){
+                return Padding(
+                  padding: EdgeInsets.all(5.0),
+                  child: Card(
+                    color: Colors.white70,
+                    child: Padding(
+                      padding: EdgeInsets.all(5.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Valor vendido: "+receitas[index].precoComercializado.toString() + "\$"+ "\n", style: TextStyle(fontSize: 16.0),),
+                          Text("Horario de venda:\n"+ dateToPdfFormat(receitas[index].horarioComercializado)+" "+timeToPdfFormat(receitas[index].horarioComercializado)+ "\n", style: TextStyle(fontSize: 16.0),),
+                          Text("Horario feito:\n"+dateToPdfFormat(receitas[index].horarioFeito)+" "+timeToPdfFormat(receitas[index].horarioFeito)+"\n", style: TextStyle(fontSize: 16.0),),
+                          Text("Custo:"+receitas[index].precoReceita.toString()+"\$"+ "\n" , style: TextStyle(fontSize: 16.0),),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ],
+        )
+      ),
+      actions: [
+        FlatButton(
+          child: Text("Ok"),
+          onPressed: (){
+            Navigator.pop(context);
+          },
+        ),
+      ],
+    );
+  }
+
+  String dateToPdfFormat(String date){
+  DateTime time = DateTime.parse(date);
+  String dia = time.day.toString();
+  if(dia.length<2)
+    dia = '0' + dia;
+  String mes = time.month.toString();
+  if(mes.length<2)
+    mes = '0' + mes;
+  String ano = time.year.toString();
+  if(ano.length<2)
+    ano = '0' + ano;
+  return dia + '/' + mes + '/' + ano; }
+
+  String timeToPdfFormat(String date){
+  DateTime time = DateTime.parse(date);
+  String hrs = time.hour.toString();
+  if(hrs.length<2)     hrs = '0' + hrs;
+    String min = time.minute.toString();
+  if(min.length<2)
+    min = '0' + min;
+  return hrs + ':' + min; }
+
 
   @override
   Widget build(BuildContext context) {
